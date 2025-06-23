8136d3dfa6a36f70a518d0329714001c540afb12
Deepak-Das-7 <deepak.d@feniceenergy.com>
2025-06-24 01:47:17 +0530

    added api

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,13 +1,12 @@
 import axios from "axios";
-
 const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
 export const fetchCode = async (roomId: string) => {
-  const res = await axios.get(`${BASE_URL}/code/${roomId}`);
+  const res = await axios.get(`${BASE_URL}/api/code/${roomId}`);
   return res.data.code || "";
 };
 
 export const saveCode = async (roomId: string, code: string) => {
-  const res = await axios.post(`${BASE_URL}/code`, { roomId, code });
+  const res = await axios.post(`${BASE_URL}/api/code`, { roomId, code });
   return res.data;
 };
